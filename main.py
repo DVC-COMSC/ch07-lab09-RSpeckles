1cd8c972f5330807a9aff845f53ebee415b5676d
RSpeckles <rchenspeckman@gmail.com>
2022-10-05 14:35:40 -0700

    Completed Lab

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 # ******************************
 # Make your Code
 # ******************************
+userInputs = input("Enter some names: ")
+
+names = list(userInputs.split())
+
+shortest = names[0]
+longest = names[0]
+
+for i in range(1, len(names)):
+    if len(names[i]) < len(shortest):
+        shortest = names[i]
+    elif len(names[i]) > len(longest):
+        longest = names[i]
+    elif len(names[i]) == len(shortest):
+        shortest = min(names[i], shortest)
+    elif len(names[i]) == len(longest):  
+        longest = min(names[i], longest)
+
+print(shortest, longest)
